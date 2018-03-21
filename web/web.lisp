87152f63bcc545aa532b60adfce36a2008c313be
gingeralesy <gingeralesy@gmail.com>
2018-03-21 18:30:16 +0200

    Actually check if it's bound

--- web/web.lisp
+++ web/web.lisp
@@ -1,6 +1,6 @@
 (in-package #:cl-twitch-web)
 
-(unless (find-symbol "+AUTH-URI-FORMAT+") ;; FIXME: Why does this error otherwise?
+(unless (boundp (find-symbol "+AUTH-URI-FORMAT+")) ;; FIXME: Why does this error otherwise?
   (defconstant +auth-uri-format+ "https://id.twitch.tv/oauth2/authorize?client_id=~a&redirect_uri=~a&response_type=code&scope=~a"))
 
 (define-page index "twitchbot/$" (:clip "dashboard.ctml")
