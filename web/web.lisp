191c2e5b546479aeacc0c67057c954bc43ba8d55
gingeralesy <gingeralesy@gmail.com>
2018-03-21 18:31:43 +0200

    Actually let's use a parameter

--- web/web.lisp
+++ web/web.lisp
@@ -1,12 +1,11 @@
 (in-package #:cl-twitch-web)
 
-(unless (boundp (find-symbol "+AUTH-URI-FORMAT+")) ;; FIXME: Why does this error otherwise?
-  (defconstant +auth-uri-format+ "https://id.twitch.tv/oauth2/authorize?client_id=~a&redirect_uri=~a&response_type=code&scope=~a"))
+(defparameter *auth-uri-format* "https://id.twitch.tv/oauth2/authorize?client_id=~a&redirect_uri=~a&response_type=code&scope=~a")
 
 (define-page index "twitchbot/$" (:clip "dashboard.ctml")
   (r-clip:process
    T
-   :tokens-url (format NIL +auth-uri-format+ (client-id) (redirect-url) (scope))))
+   :tokens-url (format NIL *auth-uri-format* (client-id) (redirect-url) (scope))))
 
 (define-page oauth "twitchbot/oauth" ()
   (get-tokens (post/get "code"))
