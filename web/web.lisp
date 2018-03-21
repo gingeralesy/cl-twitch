7ced72560de4f9e059098cd895c6c28c4341b32a
gingeralesy <gingeralesy@gmail.com>
2018-03-21 17:26:18 +0200

    Readability fix

--- web/web.lisp
+++ web/web.lisp
@@ -1,8 +1,11 @@
 (in-package #:cl-twitch-web)
 
+(defconstant +auth-uri-format+ "https://id.twitch.tv/oauth2/authorize?client_id=~a&redirect_uri=~a&response_type=code&scope=~a")
+
 (define-page index "twitchbot/$" (:clip "dashboard.ctml")
-  (r-clip:process T :tokens-url (format NIL "https://id.twitch.tv/oauth2/authorize?client_id=~a&redirect_uri=~a&response_type=code&scope=~a"
-                                        (client-id) (redirect-url) (scope))))
+  (r-clip:process
+   T :tokens-url
+   (format NIL +auth-uri-format+ (client-id) (redirect-url) (scope))))
 
 (define-page oauth "twitchbot/oauth" ()
   (get-tokens (post/get "code"))
